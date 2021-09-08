8a74efddf62138354d1057e93ee4d7d87e9df673
Nir Kahana <>
2021-09-08 17:49:07 +0300

    Returned List.js to Class component

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { Component } from 'react'
 import Modal from './Modal'
 import '../style/list.css'
 const M = window.M
 
 
-export default function List(props) {
+class List extends Component {
+    componentDidMount() {
+        M.Modal.init(this.instance.querySelectorAll('.modal'))
+    }
 
-    useEffect(() => {
-      M.Modal.init(this.instance.querySelectorAll('.modal'));
-    }, []);
-
-    const open = () => {
+    open = () => {
         let modal = this.instance.getElementsByClassName('modal')[0]
         let twtr = this.instance.getElementsByClassName('twtr')[0]
         if(twtr) twtr.className = 'twitter-timeline'
@@ -18,15 +17,19 @@ export default function List(props) {
         M.Modal.getInstance(modal).open()
     }
 
-        let item = props.item
+    render(){
+        let item = this.props.item
         let key = (item.category + item.subcategory + item.name).replace(/[^a-zA-Z0-9]/g,'')
 
         return(
             <span ref={el => (this.instance = el)}>
-                <div className="background-white company-list inline-block center cursor-pointer" onClick={open}>
+                <div className="background-white company-list inline-block center cursor-pointer" onClick={this.open}>
                     <img className="logo-list" src={item.logo} alt="logo"></img>
                 </div>
                 <Modal id={key} item={item}/>
             </span>
         )
+    }
 }
+
+export default List
