3410ce364d56c51e920da8622c1d1d09511878e7
NirK8 <nir@moveohls.com>
2021-10-11 21:05:33 +0300

    updated mobile images

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import ReactLoading from 'react-loading';
-import { useMediaQuery } from '@material-ui/core';
+import ReactLoading from "react-loading";
+import { useMediaQuery } from "@material-ui/core";
 
 import "../style/analytics.css";
 import MetaTags from "../components/MetaTags";
@@ -10,21 +10,21 @@ import totalImage from "../assets/analytics/desktop/img_2_total.jpg";
 import exitsImage from "../assets/analytics/desktop/img_3_exits.jpg";
 import lifespanImage from "../assets/analytics/desktop/img_4_lifespan.jpg";
 // mobile images
-import categoryImageMobile from "../assets/analytics/mobile/img_1_category.jpg";
-import totalImageMobile from "../assets/analytics/mobile/img_2_total.jpg";
-import exitsImageMobile from "../assets/analytics/mobile/img_3_exits.jpg";
-import lifespanImageMobile from "../assets/analytics/mobile/img_4_lifespan.jpg";
+import categoryImageMobile from "../assets/analytics/mobile/mob_img_1_category.jpg";
+import totalImageMobile from "../assets/analytics/mobile/mob_img_2_total.jpg";
+import exitsImageMobile from "../assets/analytics/mobile/mob_img_3_exits.jpg";
+import lifespanImageMobile from "../assets/analytics/mobile/mob_img_4_lifespan.jpg";
 
 const assets = [
   {
     name: "category",
     image: categoryImage,
-    mobileImage: categoryImageMobile
+    mobileImage: categoryImageMobile,
   },
   {
     name: "total",
     image: totalImage,
-    mobileImage: totalImageMobile
+    mobileImage: totalImageMobile,
   },
   {
     name: "exits",
@@ -36,59 +36,83 @@ const assets = [
     image: lifespanImage,
     mobileImage: lifespanImageMobile,
   },
-]
+];
 
 export default function Analytics(prop) {
+  const [loadedImages, setLoadedImages] = useState([]);
+  const isMobile = useMediaQuery("(max-width:768px)");
 
-  const [loadedImages, setLoadedImages] = useState([]); 
-  const isMobile = useMediaQuery('(max-width:768px)');
+  const getImage = (asset) => (isMobile ? asset.mobileImage : asset.image);
 
-  const getImage = (asset) => isMobile ? asset.mobileImage : asset.image; 
- 
   // Indicates whether all images are loaded
-  const allImagesLoaded = (loadedImages.length === 4);
+  const allImagesLoaded = loadedImages.length === 4;
 
-  const isImageLoaded = (imageName) => loadedImages.some(name => name === imageName);
+  const isImageLoaded = (imageName) =>
+    loadedImages.some((name) => name === imageName);
 
   const onImageLoad = (imageName) => {
-    !isImageLoaded(imageName) && 
-    setLoadedImages(prevLoadedImages => prevLoadedImages.concat([imageName]));
-  }
+    !isImageLoaded(imageName) &&
+      setLoadedImages((prevLoadedImages) =>
+        prevLoadedImages.concat([imageName])
+      );
+  };
   return (
     <div className="careers-view background-dark-grey flex column align-center">
       <MetaTags />
-      {(!allImagesLoaded) && <ReactLoading type={"spinningBubbles"} color={"#a79b9b"} height={'75px'} width={'75px'} className="react-loading"/>}
-      {allImagesLoaded &&
-      <div className="container-fluid">
-        <h1>CyberMap Analytics</h1>
-        <div className="row">
-          <div className="col-12 s12 m12 l8">
-            <p className="text-justify">
-              Explore YL Ventures’ CyberMap analytics to gain in-depth
-              perspectives of the Israeli cybersecurity ecosystem. Track the
-              industry's movements and market trends with our data-driven
-              findings of solution categories, funding rounds, exits, and more!
+      {!allImagesLoaded && (
+        <ReactLoading
+          type={"spinningBubbles"}
+          color={"#a79b9b"}
+          height={"75px"}
+          width={"75px"}
+          className="react-loading"
+        />
+      )}
+      {allImagesLoaded && (
+        <div className="container-fluid">
+          <h1>CyberMap Analytics</h1>
+          <div className="row">
+            <div className="col-12 s12 m12 l8">
+              <p className="text-justify">
+                Explore YL Ventures’ CyberMap analytics to gain in-depth
+                perspectives of the Israeli cybersecurity ecosystem. Track the
+                industry's movements and market trends with our data-driven
+                findings of solution categories, funding rounds, exits, and
+                more!
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {assets.map((asset) => (
+        <div
+          key={asset.name}
+          className="image-container"
+          style={allImagesLoaded ? {} : { display: "none" }}
+        >
+          <img
+            src={getImage(asset)}
+            className="analytics-image"
+            onLoad={() => {
+              onImageLoad(asset.name);
+            }}
+            alt={asset.name}
+          />
+        </div>
+      ))}
+
+      {allImagesLoaded && (
+        <div className="an-footer">
+          <div className="container-fluid">
+            <p>
+              <i className="material-icons">info_outline</i>
+              Data included refers to accessible public information. Undisclosed
+              were not included in this analys
             </p>
           </div>
         </div>
-      </div>}
-    
-     {assets.map(asset => (
-      <div key={asset.name} className="image-container" style={allImagesLoaded ? {} : { display: 'none' }}>
-        <img src={getImage(asset)} className="analytics-image" onLoad={() => {onImageLoad(asset.name)}} alt={asset.name}/>
-      </div>
-     ))}
-
-      {allImagesLoaded &&
-      <div className="an-footer">
-        <div className="container-fluid">
-          <p>
-            <i className="material-icons">info_outline</i>
-            Data included refers to accessible public information. Undisclosed
-            were not included in this analys
-          </p>
-        </div>
-      </div>}
+      )}
     </div>
   );
 }
